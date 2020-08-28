e4856be1a81f396100b657a100f74893d9c8feb4
mobal <the.mobal@gmail.com>
2020-08-28 22:22:38 +0200

    Initial commit

---
+++ src/main/kotlin/hu/netcode/springcloudstreamkotlindemo/SpringCloudStreamKotlinDemoApplication.kt
@@ -0,0 +1,11 @@
+package hu.netcode.springcloudstreamkotlindemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudStreamKotlinDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringCloudStreamKotlinDemoApplication>(*args)
+}
