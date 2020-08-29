02efbf741d984b5d585e4d15d141b80c55a5b2d0
mobal <the.mobal@gmail.com>
2020-08-29 23:06:24 +0200

    Removed unused annotation

--- src/main/kotlin/hu/netcode/springcloudstreamkotlindemo/messaging/Producer.kt
+++ src/main/kotlin/hu/netcode/springcloudstreamkotlindemo/messaging/Producer.kt
@@ -3,7 +3,6 @@ package hu.netcode.springcloudstreamkotlindemo.messaging
 import org.slf4j.LoggerFactory
 import org.springframework.cloud.stream.annotation.EnableBinding
 import org.springframework.messaging.Message
-import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.stereotype.Component
 
 @Component
@@ -13,7 +12,6 @@ class Producer(
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    @SendTo(value = [Source.REVERSED])
     fun sendToReversed(msg: Message<String>) {
         logger.info("""Sending reversed message "{}"""", msg)
         source.reversed().send(msg)
